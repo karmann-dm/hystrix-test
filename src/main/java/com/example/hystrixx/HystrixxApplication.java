4a0fd61740755053f2c4bad5dc9b8a2c8fff8ea2
karmann-dm <karmanno@gmail.com>
2021-09-07 16:48:45 +0200

    Eliminate ribbon

--- src/main/java/com/example/hystrixx/HystrixxApplication.java
+++ src/main/java/com/example/hystrixx/HystrixxApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableCircuitBreaker
 public class HystrixxApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/hystrixx/ServiceClient.java
+++ src/main/java/com/example/hystrixx/ServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "albums", fallback = ServiceFallback.class)
+@FeignClient(name = "albums", url = "https://abcd.ef", fallback = ServiceFallback.class)
 public interface ServiceClient {
 
     @GetMapping("/users/{id}/albums")
